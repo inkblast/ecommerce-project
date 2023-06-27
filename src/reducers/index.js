5220b5537c46fe212fc7be805fa16b7724401ff6
inkblast <91331458+inkblast@users.noreply.github.com>
2023-06-27 14:15:35 +0530

    Revert "Admin penel"

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import orderManagementReducer from './orderManagementReducer'
-
-const rootReducer = combineReducers({
-    product: orderManagementReducer,
-});
-
-export default rootReducer;
